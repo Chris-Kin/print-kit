169601a463799bd3c91ca8b4c0afa85f6536b528
mingyan.yu <mingyan.yu@wormpex.com>
2019-04-22 19:11:38 +0800

    feat:merge-es5

--- src/FillMode.js
+++
@@ -1,3 +0,0 @@
-export const FULL_FILL = 1; // 全部填充了
-export const SOME_FILL = 2; // 部分填充了
-export const NOTHING_FILL = 3; // 没有填充任何东西
--- src/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  // A4纸可用空间高度
-  PageHeight: 1080,
-}
